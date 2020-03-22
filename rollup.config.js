57019d64d25fab2c690cbc1a588f0be615d2879f
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-03-22 09:48:53 +0100

    Show file size after bundline scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
@@ -9,5 +10,6 @@ export default {
     commonjs(),
     babel(),
     terser(),
+    filesize(),
   ],
 };
